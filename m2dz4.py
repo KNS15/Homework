67404cf397dcde618ddd092f76e6b5e91652b710
KNS15 <167962653+KNS15@users.noreply.github.com>
2024-05-04 16:51:15 +0400

    Update m2dz4.py

--- m2dz4.py
+++ m2dz4.py
@@ -1,8 +1,7 @@
 cars = ["BMW", "MB", "LADA", "KIA", "HONDA"]
 
 for i in cars:
-    print("я езжу на автомобиле марки,", i)
-cars_count = 0
-for i in range(cars_count + 10):
-    print(i)
+    cars_count = 0
+    for j in range(cars_count + 10):
+        print("я езжу на автомобиле марки,", i)
     
