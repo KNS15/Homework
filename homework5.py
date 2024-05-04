6052ce665ae32043d9500c6103fc9071a632c96a
KNS15 <167962653+KNS15@users.noreply.github.com>
2024-05-04 11:57:14 +0400

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,11 @@
+my_list = ["apple", "kiwi", "raspberry", "banana", "orange", "coconut"]
+print(my_list)
+print(my_list[0], my_list[-1])
+print(my_list[3:5])
+my_list[3] = "apple"
+print(my_list)
+my_dict = {"paint": "рисовать", "pen": "ручка", "car": "машина", "phone": "телефон"}
+print(my_dict)
+print(my_dict["paint"])
+my_dict["paint"] = "красить";my_dict.update({"drive": "водить"})
+print(my_dict)
