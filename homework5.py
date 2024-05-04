9af192ff4499cd14f375fce278e33db6dc93ef24
KNS15 <167962653+KNS15@users.noreply.github.com>
2024-05-04 13:00:25 +0400

    Update homework5.py

--- homework5.py
+++ homework5.py
@@ -1,8 +1,8 @@
 my_list = ["apple", "kiwi", "raspberry", "banana", "orange", "coconut"]
 print(my_list)
 print(my_list[0], my_list[-1])
-print(my_list[3:5])
-my_list[3] = "apple"
+print(my_list[2:5])
+my_list[2] = "apple"
 print(my_list)
 my_dict = {"paint": "рисовать", "pen": "ручка", "car": "машина", "phone": "телефон"}
 print(my_dict)
