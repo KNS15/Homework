dd79e37ab2fe222d5fb66fd281799f486b95beb4
KNS15 <167962653+KNS15@users.noreply.github.com>
2024-09-22 13:56:16 +0400

    M13dz6.py

--- M13dz6.py
+++ M13dz6.py
@@ -1,5 +1,3 @@
-from gettext import textdomain
-
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import State, StatesGroup
